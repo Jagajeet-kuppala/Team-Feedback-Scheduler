fe9e217b5057d37f53622bd8ba3580875e37080d
Jagajeet Kuppala <jagajeet.kuppala@thoughtworks.com>
2021-06-08 14:22:56 +0530

    [Jagajeet/Keerthana] Add slots & slots handler in FeedbackScheduler

--- processFeedbackForm.js
+++ processFeedbackForm.js
@@ -1,5 +1,6 @@
 function processFeedbackForm(formObject) {
     try {
+        Logger.log(formObject);
         const {users, startDate, startTime, endTime, userDateTime} = formObject;
         const userList = getValidUsers(users);
         validateDateTime(startDate, startTime, endTime, userDateTime);
@@ -14,25 +15,33 @@ function processFeedbackForm(formObject) {
 }
 
 function validateDateTime(startDate, startTime, endTime, userDateTime) {
-    const moment = new Date();
-    const todaysDate = moment.toISOString().split('T')[0];
+    if(startTime.length !== endTime.length) {
+        throw new Error("NUMBER_OF_START_TIMES_SHOULD_EQUAL_END_TIMES");
+    }
+    const moment = new Date(userDateTime);
     const userTimeZoneOffset = new Date(userDateTime).getTimezoneOffset();
     const hoursOffset = userTimeZoneOffset / 60;
     const minutesOffset = userTimeZoneOffset % 60;
 
     moment.setHours(moment.getHours() - hoursOffset);
     moment.setMinutes(moment.getMinutes() - minutesOffset);
+    const todaysDate = moment.toISOString().split('T')[0];
     const currentUserTime = moment.toISOString().split('T')[1].slice(0, 5);
 
     if (startDate < todaysDate) {
         throw new Error("START_DATE_SHOULD_NOT_BE_IN_PAST");
     }
     if (startDate === todaysDate) {
-        if (startTime < currentUserTime) {
-            throw new Error("START_TIME_SHOULD_NOT_BE_IN_THE_PAST");
+        for(let itr = 0; itr < startTime.length; startTime++) {
+            if (startTime[itr] < currentUserTime) {
+                throw new Error("START_TIME_SHOULD_NOT_BE_IN_THE_PAST");
+            }
         }
     }
-    if (endTime < startTime) {
-        throw new Error("END_TIME_SHOULD_BE_GREATER_THAN_START_TIME");
+    for(let itr = 0; itr < startTime.length; startTime++) {
+        if (endTime < startTime) {
+            throw new Error("END_TIME_SHOULD_BE_GREATER_THAN_START_TIME");
+        }
     }
+    Logger.log("All Date & Times are valid");
 }
--- scheduleEvents.js
+++ scheduleEvents.js
@@ -1,27 +1,42 @@
-function scheduleEvents(userPairs, startDate, startTime, endTime) {
-    let startDateTimeObj = dateTimeBuilder(startDate, startTime);
-    let endDateTimeObj = dateTimeBuilder(startDate, endTime);
-    if (isWeekend(startDateTimeObj)) {
-        startDateTimeObj = getNextWorkingDay(startDateTimeObj);
-        endDateTimeObj = getNextWorkingDay(endDateTimeObj);
-    }
+function scheduleEvents(userSlotPairs, startDate, startTime, endTime) {
+    const slots = getDateTimeSlots(userSlotPairs.length, startDate, startTime, endTime);
 
-    for (let i = 0; i < userPairs.length; i++) {
-        for (let j = 0; j < userPairs[i].length; j++) {
-            if (!hasDummyUser(userPairs[i][j])) {
-                createCalendarEvent(userPairs[i][j][0], userPairs[i][j][1], startDateTimeObj.toISOString(), endDateTimeObj.toISOString());
+    for (let i = 0, slot = 0; i < userSlotPairs.length; i++, slot++) {
+        for (let j = 0; j < userSlotPairs[i].length; j++) {
+            if (!hasDummyUser(userSlotPairs[i][j])) {
+                createCalendarEvent(
+                    userSlotPairs[i][j][0], userSlotPairs[i][j][1],
+                    slots[slot].startDateTimeObj.toISOString(), slots[slot].endDateTimeObj.toISOString()
+                );
             }
         }
-        startDateTimeObj = getNextWorkingDay(startDateTimeObj);
-        endDateTimeObj = getNextWorkingDay(endDateTimeObj);
     }
 }
 
-function dateTimeBuilder(date, time) {
-    const dateTimeObj = new Date(date);
-    const splitTime = time.split(':');
-    dateTimeObj.setHours(splitTime[0], splitTime[1], 0);
+function getDateTimeSlots(slotCount, startDate, startTime, endTime) {
+    const dateTimeSlots = [];
+    startDate = dateTimeBuilder(startDate);
+    if (isWeekend(startDate)) {
+        startDate = getNextWorkingDay(startDate);
+    }
+    for (let slot = 0; slot < slotCount;) {
+        for (let itr = 0; itr < startTime.length && slot < slotCount; itr++, slot++) {
+            let startDateTimeObj = dateTimeBuilder(startDate, startTime[itr]);
+            let endDateTimeObj = dateTimeBuilder(startDate, endTime[itr]);
 
+            dateTimeSlots.push({startDateTimeObj, endDateTimeObj});
+        }
+        startDate = getNextWorkingDay(startDate);
+    }
+    return dateTimeSlots;
+}
+
+function dateTimeBuilder(date, time = null) {
+    const dateTimeObj = new Date(date);
+    if(time) {
+        const splitTime = time.split(':');
+        dateTimeObj.setHours(splitTime[0], splitTime[1], 0);
+    }
     return dateTimeObj;
 }
 
